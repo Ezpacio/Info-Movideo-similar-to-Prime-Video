2470016a3f4b9d7ab3de1be7f9891ad65408a987
Samet Gokkaya <sametgokkayax@gmail.com>
2024-01-19 23:55:03 +0300

    added net pages and js files

--- dist/Js/main.js
+++ dist/Js/main.js
@@ -1,113 +1,24 @@
-const navBtns = document.querySelectorAll('.nav-li');
-const searchBtn = document.querySelector('.search-icon');
-const resetBtn = document.querySelector('input[type="reset"]');
-const tabletMenu = document.getElementById('menu');
-const listToggle = document.querySelector('.kategori');
-const profile = document.getElementById('profile');
-
-// add sticky class to navigation
-window.addEventListener('scroll', () => {
-    document.querySelector('header').classList.toggle('sticky', window.scrollY > 60);
-    document.querySelector('.flip-top').classList.toggle('active', window.scrollY > 650)
-})
-
-// Add active class navigation buttons
-const navigationButn = () => {
-    navBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            removeActiveClasses();
-            btn.classList.add('active');
-            tabletMenu.classList.remove('active')
-        })
-    })
-    const removeActiveClasses = () => {
-        navBtns.forEach(item => {
-            item.classList.remove('active')
-        })
+const global = {
+    currentPage: window.location.pathname,
+    api: {
+        api_key: 'd087f4c33449d2dc1505b6a5b29961e1',
+        api_url: 'https://api.themoviedb.org/3/'
     }
-};
-
-document.querySelectorAll('.t-list').forEach(item => {
-    item.addEventListener('click', () => {
-        tabletMenu.classList.remove('active');
-    })
-})
-document.querySelectorAll('.c-list').forEach(item => {
-    item.addEventListener('click', () => {
-        profile.classList.remove('active');
-    })
-})
-
-
-
-
-
-
-/* Slide Options*/
-const swiper = new Swiper('.swiper', {
-    speed: 900,
-    spaceBetween: 30,
-    slidesPerView: 1,
-    pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true
-    },
-    keyboard:{
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true
-    },
-    autoplay: {
-        delay: 5000,
-    },
-    // effect: 'coverflow',
-    // grabCursor: true,
-    // centeredSlides: true,
-    // loop: true,
-    // coverflowEffect: {
-    // rotate: 0,
-    // stretch: 0,
-    // depth: 100,
-    // modifier: 2.5,
-    // slideShadows: true,
-    // },
-});
-
-
-
-
-
-function init() {
-
-    searchBtn.addEventListener('click', () => {
-        // searchBtn.firstElementChild.classList.toggle('iconoir-xmark');
-        // searchBtn.parentElement.classList.remove('active');
-    });
-    resetBtn.addEventListener('click', () => {
-        resetBtn.parentElement.previousElementSibling.value = '';
-    });
-    tabletMenu.addEventListener('click', () => {
-        tabletMenu.classList.toggle('active')
-        tabletMenu.lastElementChild.classList.toggle('rotate'); // Icon
-    });
-    searchBtn.addEventListener('click', () => {
-        searchBtn.parentElement.classList.toggle('active');
-        searchBtn.firstElementChild.classList.toggle('iconoir-xmark')
-    });
-    listToggle.addEventListener('click', () => {
-        searchBtn.classList.toggle('active');
-    });
-    document.querySelector('#navigation').addEventListener('click', function(event) {
-        event.stopPropagation(); 
-    });
-    profile.addEventListener('click', () => {
-        profile.classList.toggle('active');
-    })
-    document.querySelector('.profile-popup').addEventListener('click', function(event) {
-        event.stopPropagation(); 
-    });
-
-    navigationButn();
+    
 }
-init();
+
+const init = ()=>{
+    switch (global.currentPage){
+        case '/dist/index.html':
+            console.log('AnaSayfa');
+            break;
+        case '/dist/movies.html':
+            console.log('Filmler');
+            break;
+        case '/dist/shows.html':
+            console.log('TV Dizileri');
+            break;
+    
+    }
+}
+document.addEventListener('DOMContentLoaded', init);
---
+++ dist/Js/script.js
@@ -0,0 +1,113 @@
+const navBtns = document.querySelectorAll('.nav-li');
+const searchBtn = document.querySelector('.search-icon');
+const resetBtn = document.querySelector('input[type="reset"]');
+const tabletMenu = document.getElementById('menu');
+const listToggle = document.querySelector('.kategori');
+const profile = document.getElementById('profile');
+
+// add sticky class to navigation
+window.addEventListener('scroll', () => {
+    document.querySelector('header').classList.toggle('sticky', window.scrollY > 60);
+    document.querySelector('.flip-top').classList.toggle('active', window.scrollY > 650)
+})
+
+// Add active class navigation buttons
+const navigationButn = () => {
+    navBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            removeActiveClasses();
+            btn.classList.add('active');
+            tabletMenu.classList.remove('active')
+        })
+    })
+    const removeActiveClasses = () => {
+        navBtns.forEach(item => {
+            item.classList.remove('active')
+        })
+    }
+};
+
+document.querySelectorAll('.t-list').forEach(item => {
+    item.addEventListener('click', () => {
+        tabletMenu.classList.remove('active');
+    })
+})
+document.querySelectorAll('.c-list').forEach(item => {
+    item.addEventListener('click', () => {
+        profile.classList.remove('active');
+    })
+})
+
+
+
+
+
+
+/* Slide Options*/
+const swiper = new Swiper('.swiper', {
+    speed: 900,
+    spaceBetween: 30,
+    slidesPerView: 1,
+    pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+    },
+    keyboard:{
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    // effect: 'coverflow',
+    // grabCursor: true,
+    // centeredSlides: true,
+    // loop: true,
+    // coverflowEffect: {
+    // rotate: 0,
+    // stretch: 0,
+    // depth: 100,
+    // modifier: 2.5,
+    // slideShadows: true,
+    // },
+});
+
+
+
+
+
+function initAll() {
+
+    searchBtn.addEventListener('click', () => {
+        // searchBtn.firstElementChild.classList.toggle('iconoir-xmark');
+        // searchBtn.parentElement.classList.remove('active');
+    });
+    resetBtn.addEventListener('click', () => {
+        resetBtn.parentElement.previousElementSibling.value = '';
+    });
+    tabletMenu.addEventListener('click', () => {
+        tabletMenu.classList.toggle('active')
+        tabletMenu.lastElementChild.classList.toggle('rotate'); // Icon
+    });
+    searchBtn.addEventListener('click', () => {
+        searchBtn.parentElement.classList.toggle('active');
+        searchBtn.firstElementChild.classList.toggle('iconoir-xmark')
+    });
+    listToggle.addEventListener('click', () => {
+        searchBtn.classList.toggle('active');
+    });
+    document.querySelector('#navigation').addEventListener('click', function(event) {
+        event.stopPropagation(); 
+    });
+    profile.addEventListener('click', () => {
+        profile.classList.toggle('active');
+    })
+    document.querySelector('.profile-popup').addEventListener('click', function(event) {
+        event.stopPropagation(); 
+    });
+
+    navigationButn();
+}
+initAll();
